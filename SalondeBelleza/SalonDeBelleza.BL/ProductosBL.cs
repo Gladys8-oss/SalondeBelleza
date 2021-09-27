11085061802e687350dffaeb9e09c02c04e715e9
Gladys <Gladys@GLADYSSOSA>
2021-09-27 17:17:54 -0600

    Agregando arquitectura basica

---
+++ SalondeBelleza/SalonDeBelleza.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonDeBelleza.BL
+{
+    public class ProductosBL
+    {
+      public   List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Plancha";
+            producto1.Precio = 600;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Labial";
+            producto2.Precio = 200;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Sombras";
+            producto3.Precio = 900;
+
+            var ListadeProductos = new List<Producto>();
+            ListadeProductos.Add(producto1);
+            ListadeProductos.Add(producto2);
+            ListadeProductos.Add(producto3);
+
+            return ListadeProductos;
+        }
+    }
+}
--- SalondeBelleza/SalondeBelleza.web/Controllers/ProductosController.cs
+++ SalondeBelleza/SalondeBelleza.web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using SalondeBelleza.web.Models;
+﻿using SalonDeBelleza.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +12,10 @@ namespace SalondeBelleza.web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Plancha";
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Labial";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Sombras";
-
-            var ListadeProductos = new List<ProductoModel>();
-            ListadeProductos.Add(producto1);
-            ListadeProductos.Add(producto2);
-            ListadeProductos.Add(producto3);
-
-            return View(ListadeProductos);
+            return View(listadeProductos);
         }
     }
 }
