307f378d97bb1a0133d7c75d43be4c61e54b55d6
Gladys <Gladys@GLADYSSOSA>
2021-11-06 19:22:59 -0600

    Subiendo cambios Filtros de ordenamiento

--- SalondeBelleza/SalondeBelleza.Win/Form2.cs
+++ SalondeBelleza/SalondeBelleza.Win/Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SalonDeBelleza.BL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace SalondeBelleza.Win
 {
     public partial class Form2 : Form
     {
+        ReportedeVentasPorProductoBL _reporteVentasPorProductoBL;
+
         public Form2()
         {
+
+
+            _reporteVentasPorProductoBL = new ReportedeVentasPorProductoBL();
+
+            // RefrescarDatos();
             InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefrescarDatos();
+        }
+
+        private void RefrescarDatos()
+        {
+            var listadeVentasPorProducto = _reporteVentasPorProductoBL.ObtenerVentasPorProducto();
+            listadeVentasPorProductoBindingSource.DataSource = listadeVentasPorProducto;
+            listadeVentasPorProductoBindingSource.ResetBindings(false);
         }
     }
 }
